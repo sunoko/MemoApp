3a826d88d67d7c73927fe5dbe91c0f4c94832959
Kazuki Chinone <staybesidehangon@gmail.com>
2019-08-25 11:06:21 +0900

    login and signup screen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  StyleSheet, View, Text, TextInput, TouchableHighlight,
+} from 'react-native';
+
+class LoginScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>ログイン</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <TouchableHighlight style={styles.button} onPress={() => {}} underlayColor="#c70f66">
+          <Text style={styles.buttonTitle}>ログインする</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 24,
+    borderColor: 'white',
+  },
+  input: {
+    backgroundColor: '#eee',
+    height: 40,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 8,
+  },
+  title: {
+    fontSize: 28,
+    alignSelf: 'center',
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: '#e31676',
+    height: 48,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '70%',
+  },
+  buttonTitle: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
+
+export default LoginScreen;
