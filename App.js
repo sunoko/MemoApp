e5aeedab63230bce2c97aea802c7700c5adfbcb3
Kazuki Chinone <staybesidehangon@gmail.com>
2019-08-25 09:16:29 +0900

    memolistscreen

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Appbar from './src/components/Appbar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/elements/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>      
+    <View style={styles.container}>
       <Appbar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
+      <MemoListScreen />
     </View>
   );
 }
